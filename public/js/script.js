ee0ab84eb2c0b24b706853f93d445329f34eecf1
Sébastien Herbreteau <seb.eric.herbreteau@gmail.com>
2023-07-25 17:33:24 +0200

    Suppression js image

--- public/js/script.js
+++
@@ -1,6 +0,0 @@
-let addImages = document.querySelector('#add-image');
-let imagesContainer = document.querySelector('#ad_images')
-addImages.addEventListener('click', () => {
-    const index = imagesContainer.getElementsByClassName('form-group').length;
-    imagesContainer.innerHTML += (imagesContainer.dataset.prototype).replace(/__name__/g, index);
-
